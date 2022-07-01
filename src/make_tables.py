9c63dbebcf1d1fb1f8dc48f280dc05ff67eaec03
Bas Terwijn <bterwijn@gmail.com>
2022-07-02 00:21:47 +0200

    update

--- src/make_tables.py
+++ src/make_tables.py
@@ -2,9 +2,24 @@ import os
 import csv
 import sqlite3
 
+def main():
+    data_path=os.path.join("..","data")
+
+def create_data_tables(data_path):
+    sql_connection = sqlite3.connect('lectures_en_lesroosters.db')
+    sql_cursor = sql_connection.cursor()
+    create_table_rooms(os.path.join(data_path,"zalen.csv"),sql_cursor)
+    create_table_days(os.path.join(data_path,"dagen.csv"),sql_cursor)
+    create_table_timeslots(os.path.join(data_path,"tijden.csv"),sql_cursor)
+    create_table_courses(os.path.join(data_path,"vakken.csv"),sql_cursor)
+    create_table_students(os.path.join(data_path,"studenten_en_vakken.csv"),sql_cursor)
+    sql_connection.commit()
+    sql_connection.close()
+
 def create_table_rooms(filename,sql_cursor):
     sql_cursor.execute("DROP TABLE IF EXISTS rooms")
     sql_cursor.execute("CREATE TABLE rooms (name text, capacity int, special text)")
+    sql_cursor.execute("CREATE INDEX rooms_name ON rooms(name)")
     with open(filename, 'r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader) # skip first (header) row
@@ -20,6 +35,7 @@ def create_table_rooms(filename,sql_cursor):
 def create_table_days(filename,sql_cursor):
     sql_cursor.execute("DROP TABLE IF EXISTS days")
     sql_cursor.execute("CREATE TABLE days (name text)")
+    sql_cursor.execute("CREATE INDEX days_name ON days(name)")
     with open(filename, 'r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader) # skip first (header) row
@@ -33,6 +49,7 @@ def create_table_days(filename,sql_cursor):
 def create_table_timeslots(filename,sql_cursor):
     sql_cursor.execute("DROP TABLE IF EXISTS timeslots")
     sql_cursor.execute("CREATE TABLE timeslots (name text,cost int,special text)")
+    sql_cursor.execute("CREATE INDEX timeslots_name ON timeslots(name)")
     with open(filename, 'r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader) # skip first (header) row
@@ -47,57 +64,60 @@ def create_table_timeslots(filename,sql_cursor):
 
 def create_table_courses(filename,sql_cursor):
     sql_cursor.execute("DROP TABLE IF EXISTS courses")
-    sql_cursor.execute("CREATE TABLE courses (name text,nr_hoorcolleges int,nr_werkcolleges int,max_stud_werkcolleges int,nr_practica int,max_stud_practica int,expected_nr_students int)")
+    sql_cursor.execute("CREATE TABLE courses (name text,type text,max_student int,expected_nr_students)")
+    sql_cursor.execute("CREATE INDEX courses_name ON courses(name)")
     with open(filename, 'r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader) # skip first (header) row
+        unlimited=999999
         for row in csv_reader:
             if len(row)>0:
                 name=row[0]
+                expected_nr_students=row[6]
+                #print(name,expected_nr_students)
+                #------- hoorcolleges
                 nr_hoorcolleges=int(row[1])
+                for i in range(nr_hoorcolleges):
+                    sql_cursor.execute("INSERT INTO courses VALUES (?,?,?,?)",
+                                       (name,"h"+str(i+1),unlimited,expected_nr_students))
+                #------- werkcolleges
                 nr_werkcolleges=int(row[2])
-                max_stud_werkcolleges=999999
+                max_stud_werkcolleges=unlimited
                 try:
                     max_stud_werkcolleges=int(row[3])
                 except:
                     pass
+                for i in range(nr_werkcolleges):
+                    sql_cursor.execute("INSERT INTO courses VALUES (?,?,?,?)",
+                                       (name,"w"+str(i+1),max_stud_werkcolleges,expected_nr_students))
+                #------- practica
                 nr_practica=int(row[4])
-                max_stud_practica=999999
+                max_stud_practica=unlimited
                 try:
-                    max_stud_werkcolleges=int(row[5])
+                    max_stud_practica=int(row[5])
                 except:
                     pass
-                expected_nr_students=row[6]
-                #print(name,nr_hoorcolleges,nr_werkcolleges,max_stud_werkcolleges,nr_practica,max_stud_practica,expected_nr_students)
-                sql_cursor.execute("INSERT INTO courses VALUES (?,?,?,?,?,?,?)",
-                                   (name,nr_hoorcolleges,nr_werkcolleges,max_stud_werkcolleges,nr_practica,max_stud_practica,expected_nr_students))
-
+                for i in range(nr_practica):
+                    sql_cursor.execute("INSERT INTO courses VALUES (?,?,?,?)",
+                                       (name,"p"+str(i+1),max_stud_practica,expected_nr_students))
+                
 def create_table_students(filename,sql_cursor):
     sql_cursor.execute("DROP TABLE IF EXISTS students")
-    sql_cursor.execute("CREATE TABLE students (student text,course text)")
+    sql_cursor.execute("CREATE TABLE students (name text,course text)")
+    sql_cursor.execute("CREATE INDEX students_name ON students(name)")
     with open(filename, 'r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader) # skip first (header) row
         for row in csv_reader:
             if len(row)>0:
-                student=f"{row[1]} {row[0]}"
+                name=f"{row[1]} {row[0]}"
                 for index in range(3,len(row)):
                     course=row[index]
                     if len(course)<2:
                         break
                     #print(student,course)
                     sql_cursor.execute("INSERT INTO students VALUES (?,?)",
-                                   (student,course))
-        
-def main():
-    sql_connection = sqlite3.connect('lectures_en_lesroosters.db')
-    sql_cursor = sql_connection.cursor()
-    data_path=os.path.join("..","data")
-    create_table_rooms(os.path.join(data_path,"zalen.csv"),sql_cursor)
-    create_table_days(os.path.join(data_path,"dagen.csv"),sql_cursor)
-    create_table_timeslots(os.path.join(data_path,"tijden.csv"),sql_cursor)
-    create_table_courses(os.path.join(data_path,"vakken.csv"),sql_cursor)
-    create_table_students(os.path.join(data_path,"studenten_en_vakken.csv"),sql_cursor)
+                                       (name,course))
     
 if __name__ == "__main__":
     main()
